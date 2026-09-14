f29a67b3a51009bc16ff2bfb2cec745a2d32b0b4
novanira <novanira491@gmail.com>
2026-09-14 20:42:28 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # main.py
 from fastapi import FastAPI
 
+from connectors.new_world import router as search_router
+
 app = FastAPI()
+app.include_router(search_router)
 
 @app.get("/")
 async def root():
