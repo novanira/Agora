6fdd11a9d76146a5628d3170189cfcf28d219479
novanira <novanira491@gmail.com>
2026-09-23 10:06:44 +1200

    testish

--- backend/utils/text.py
+++ backend/utils/text.py
@@ -9,16 +9,18 @@ def fuzzy_similarity(text1, text2):
     num += 0.3 * fuzz.token_sort_ratio(clean_text(text1), clean_text(text2))
     return num
 
-# test_names = [
-#     ['Coca-Cola Zero Sugar Soft Drink', 'Coca-Cola Soft Drink Zero Sugar 2.25L'],
-#     ['Eta Ripple Cut Sour Cream & Chives Potato Chips', 'Eta Ripples Chips Sour Cream & Chives 150g']
-# ]
+test_names = [
+    ['Coca-Cola Zero Sugar Soft Drink', 'Coca-Cola Soft Drink Zero Sugar 2.25L'],
+    ['Eta Ripple Cut Sour Cream & Chives Potato Chips', 'Eta Ripples Chips Sour Cream & Chives 150g'],
+    ['Heinz Ketchup Tomato Sauce', 'Heinz Tomato Ketchup 500mL']
+]
 
-# def test_fuzzy_similarity(text1, text2):
-#     print(text1)
-#     print(text2)
-#     print(fuzzy_similarity(text1, text2))
-#     return True
+def test_fuzzy_similarity(text1, text2):
+    print(text1)
+    print(text2)
+    print(fuzzy_similarity(text1, text2))
+    print(fuzz.token_set_ratio(clean_text(text1), clean_text(text2)))
+    return True
 
-# for names in test_names:
-#     test_fuzzy_similarity(names[0], names[1])
+for names in test_names:
+    test_fuzzy_similarity(names[0], names[1])
